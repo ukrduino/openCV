3c15d402d8d747227472dc0902a0f2484d5f8ef0
sergey <ukrduino@gmail.com>
2020-02-27 16:46:27 +0000

    script updated to run pycharm on raspberry pi

--- run_script.sh
+++ run_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1 python3 opencv_object_tracking.py --video dashcam_boston.mp4 --tracker csrt
+LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1 /opt/pycharm-community-2019.3.3/bin/pycharm.sh
